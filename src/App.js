fdb8c745c3916a082fa0aad9741bbcd0a0888bd3
anaiva27 <nastia.iv.91@gmail.com>
2021-03-04 19:21:29 -0800

    fixed routing /about problem

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Header/>
         <Switch>
          
-          <Route exact path="/about" component={About} />
+          <Route exact path={["/", "/About"]} component={About} />
           <Route exact path="/portfolio" component={Portfolio}/>
           <Route exact path="/contact" component={Contact}/>
        
